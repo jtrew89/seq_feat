9fe7f74d757a95e6428105bd1bc71473c993e054
jtrew89 <52958532+jtrew89@users.noreply.github.com>
2024-03-01 15:28:11 +0000

    Update seq_feat.py

--- seq_feat.py
+++ seq_feat.py
@@ -133,7 +133,7 @@ def return_results(gc_count,codon_results,ind_pos):
 		for loc in results_df.index: #check if indel is in range and add position to dataframe
 			loc = str(loc) #make string to use split downstream
 			for ind in ind_pos:
-				results_df.loc[loc]['INDEL_POS'] = 0 #set initial value INDEL count
+				#results_df.loc[loc]['INDEL_POS'] = 0 #set initial value INDEL count
 				if int(loc.split('-')[0]) <= int(ind) <= int(loc.split('-')[1]): #condition for if indel position falls within the current genome location
 					results_df.loc[loc]['INDEL_POS'] = results_df.loc[loc]['INDEL_POS'] + 1 #if it and other are, add to the INDEL count for the window range
 	else:
